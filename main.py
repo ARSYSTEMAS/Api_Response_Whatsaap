be578045c9d087fc574f491cf4b066d365be91ba
ARSYSTEMAS <andersonrafael.garciagil@gmail.com>
2022-10-20 08:38:00 -0400

    nuevo archivo main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, json
+import requests
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "anderson"
+
+@app.route('/webhook', methods = ['POST', 'GET'])
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
