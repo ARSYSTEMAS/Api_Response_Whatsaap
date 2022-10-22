1a221b266bee928d052423715782e6b7d73971bb
ARSYSTEMAS <andersonrafael.garciagil@gmail.com>
2022-10-22 02:48:20 -0400

    codificado el mensaje de retorno linea 45

--- main.py
+++ main.py
@@ -27,31 +27,25 @@ def send_msg(msg):
 
 @app.route('/webhook', methods = ['POST', 'GET'])
 
-def verify():
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
-
-    return "Hello world", 200
-
-
 def webhook():
     res = request.get_json()
     print(res)
-    print(json.dumps(res, indent= 4 ))
+    print(json.dumps(res, indent=4))
     try:
         if res['entry'][0]['changes'][0]['value']['messages'][0]['id']:
             msg_body = res["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"]
             send_msg("\ud83e\udd1a gracias por responder, tu mensaje es: " + msg_body)
     except:
         pass
-    return '200 OK HTTPS.'
+    return '200'
 
+def verify():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
 
-
-
-
+    return "WebHook Configurado...", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
